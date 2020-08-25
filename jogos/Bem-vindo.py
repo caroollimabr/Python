26c9f0c5490eb0277f54fd33e2bbb2c9c6616202
caroollimabr <carool.limabr@gmail.com>
2020-08-24 22:24:37 -0300

    adivinhacao-for, break, continue e string interpolation

---
+++ jogos/Bem-vindo.py
@@ -0,0 +1,6 @@
+usuario = input("Digite seu nome:")
+
+if(usuario):
+    print("Bem-vindo, ", usuario, "!")
+else:
+    print("Usuário não identificado.")
--- jogos/adivinhacao-for.py
+++ jogos/adivinhacao-for.py
@@ -7,18 +7,24 @@ total_de_tentativas = 3
 # rodada é o nosso contador i
 rodada = 1
 
-while(rodada <= total_de_tentativas):
-    #string interpolation no python "...{}".format(variavel)
+# for variavel in range (inicial, final):
+# se o contador tiver que ir de 3 em 3, por exemplo, é preciso adicionar um step nos parênteses:
+# for rodada in range (1, total_de_tentativas+1, 3)
+for rodada in range(1, total_de_tentativas+1):
+    #string interpolation no python "...{}".format(variavel).
+    # Posso formatar {:.2f} (f de float, 1.00, por exemplo) ou {:4.2f} ou {:04.2f} (ex. 0001.00)
+    # Data {:02d}/{:02d} - numero inteiro ex. 04/09 (precisa colocar 0)
+    # {nome.lower()}: transforma nome é lower case
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     # input permite que o usuário digite algo (do tipo string)
-    chute_str = input("Digite o seu número entre 1 e 100: ")
+    chute_str = input("Digite o seu número: ")
     print("Você digitou ", chute_str)
     # transformando string em int
     chute = int(chute_str)
 
-    if(chute < 1 and chute > 100):
+    if (chute < 1 or chute > 100):
         print("Você deve digitar um número entre 1 e 100!")
-        # continue é o oposto do break
+        # continue é o oposto do break (continue a interação). Ele pode servir para pular uma iteração if (i == 5): continue
         continue
 
     acertou = numero_secreto == chute

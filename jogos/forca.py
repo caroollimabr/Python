26c60219ad70a0158ba23266c179a265dd93fc0f
caroollimabr <carool.limabr@gmail.com>
2020-09-13 18:02:09 -0300

    jogo da forca completado

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,35 +1,182 @@
+import random
 
 def jogar():
-    print("*********************************")
-    print("Bem-vindo ao jogo de forca!")
-    print("*********************************")
+    imprimir_mensagem_abertura()
+    palavra_secreta = carregar_palavra_secreta()
+    # LIST COMPREHENSIONS: coloca um "_" para cada letra da palavra
+    letras_acertadas = inicializar_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
-    palavra_secreta = "fulano"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
-
+    # False e True são valores, portanto estão em letra maiúscula
     enforcou = False
     acertou = False
-    # False e True são valores, portanto estão em letra maiúscula
+    erros = 0
 
     while(not enforcou and not acertou):
-        chute = input("Qual será a próxima letra? ")
-        #strip() retira os espaços antes e depois da palavra, caso eles existam
-        chute = chute.strip()
 
-        index = 0
-        for letra in palavra_secreta:
-            #transformo as variáveis em string maiúsculas e comparo o chute com as letras da palavra secreta
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        chute = pedir_chute()
 
-        #Aqui vai substituir os underscores
+        if(chute in palavra_secreta):
+            marcar_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            erros += 1
+            print("Eita, a palavra não possui essa letra. Faltam {} tentativas.".format(7 - erros))
+            desenhar_forca(erros)
+
+        enforcou = erros == 7
+        # Enquanto houver "_" em letras acertadas, eu ainda não acertei
+        acertou = "_" not in letras_acertadas
+        # Aqui vai substituir os underlines
         print(letras_acertadas)
 
+    if(acertou):
+        imprimir_mensagem_vencedor()
+    else:
+        imprimir_mensagem_game_over(palavra_secreta)
     print("FIM DE JOGO")
+
+# Funções do jogo
+def desenhar_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print (" |      (_)   ")
+        print (" |            ")
+        print (" |            ")
+        print (" |            ")
+
+    if(erros == 2):
+        print (" |      (_)   ")
+        print (" |      \     ")
+        print (" |            ")
+        print (" |            ")
+
+    if(erros == 3):
+        print (" |      (_)   ")
+        print (" |      \|    ")
+        print (" |            ")
+        print (" |            ")
+
+    if(erros == 4):
+        print (" |      (_)   ")
+        print (" |      \|/   ")
+        print (" |            ")
+        print (" |            ")
+
+    if(erros == 5):
+        print (" |      (_)   ")
+        print (" |      \|/   ")
+        print (" |       |    ")
+        print (" |            ")
+
+    if(erros == 6):
+        print (" |      (_)   ")
+        print (" |      \|/   ")
+        print (" |       |    ")
+        print (" |      /     ")
+
+    if (erros == 7):
+        print (" |      (_)   ")
+        print (" |      \|/   ")
+        print (" |       |    ")
+        print (" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+
+def imprimir_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
+def imprimir_mensagem_game_over(palavra_secreta):
+    print("Que pena, você foi enforcado :(")
+    print("A palavra era {}.".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+def marcar_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        # comparo o chute com as letras da palavra secreta
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1 # index = index + 1
+
+def pedir_chute():
+    chute = input("Qual será a próxima letra? ")
+    # strip() retira os espaços antes e depois da palavra, caso eles existam
+    chute = chute.strip().upper()
+    return chute
+
+def inicializar_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+def imprimir_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+def carregar_palavra_secreta():
+    arquivo = open("palavrasForca.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+# Poderia ser assim:
+# def carrega_palavra_secreta(nome_arquivo="palavras.txt", primeira_linha_valida=0):
+#     arquivo = open(nome_arquivo, "r")
+#     palavras = []
+#
+#     ….
+#
+#     numero = random.randrange(primeira_linha_valida, len(palavras))
+#     palavra_secreta = palavras[numero].upper()
+#
+#     return palavra_secreta
 
 if(__name__ == "__main__"):
     jogar()
+
+
+
+
+
+
 
 #chute = chute.strip()
 #quando variavel.find("bla") não encontra nada, ele devolve -1. Caso encontre, vai devolver a posição do elemento em questão, começando do 0.
@@ -61,3 +208,56 @@ if(__name__ == "__main__"):
 
 #SUMMING UP...
 #Coleções em Python: 1 list[indice], 2 tuple(imutavel, indice), 3 str"indice", 4 range[list indice], 5 set{nao tem valor duplicado}
+
+# LIST COMPREHENSIONS
+# Ao invés de
+# frutas = ["maçã", "banana", "laranja", "melancia"]
+# lista = []
+# for fruta in frutas:
+#     lista.append(fruta.upper())
+
+# USE AS LIST COMPREHENSIONS:
+# lista = [fruta.upper() for fruta in frutas]
+
+#inteiros = [1,3,4,5,7,8]
+#quadrados = [n*n for n in inteiros]
+
+#inteiros = [1,3,4,5,7,8,9]
+# pares = []
+# for numero in inteiros:
+#     if numero % 2 == 0:
+#         pares.append(numero)
+# ----AO INVÉS DISSO, USANDO LIST COMPREHENSION:
+# inteiros = [1,3,4,5,7,8,9]
+# pares = [x for x in inteiros if x % 2 == 0]
+
+# NO CONSOLE
+# open ("arquivo.txt", "w") - w vem de write
+# arquivo = open("arquivo.txt", "a") a vem de append
+# arquivo = open("arquivo.txt", "r") a vem de read
+# ler = arquivo.read() - Vê o que tem dentro
+# ler_linha = arquivo.readline() - Vê apenas uma linha do arquivo (leitura de modo controlado)
+# print(ler_linha)
+# arquivo.write("fulano\n") - vc está escrevendo dentro do arquivo
+# arquivo.close()
+
+# imagem = open("foto.jpg", "rb")
+# Por exemplo, o código abaixo cria uma cópia de uma imagem:
+#
+# #arquivo copia.py
+# logo = open('python-logo.png', 'rb')
+# data = logo.read() ---read() lê o arquivo inteiro de uma vez, colocando o ponteiro de leitura no final do mesmo. Se chamarmos a função read() novamente, nada será lido por causa disso.
+# logo.close()
+#
+# logo2 = open('python-logo2.png', 'wb')
+# logo2.write(data)
+# logo2.close()
+
+# sintaxe para abertura de arquivo:
+# with open("palavras.txt") as arquivo:
+#     for linha in arquivo:
+#         print(linha)
+
+# EVITE USAR VARIAVEIS GLOBAIS, O IDEAL É QUE ELA SEJA VISIVEL APENAS DENTRO DA FUNÇÃO
+# PENSE NA VIABILIDADE DA FUNÇÃO: SE ELA RETORNA POUCA COISA, TALVEZ VALHA A PENA DEIXÁ-LA JUNTO AO CÓDIGO
+# Funções são importantes também para dar melhor visibilidade, teste e manutenção do código

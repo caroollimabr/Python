fb8f23287336322b93d561bef46c81b46f34a486
caroollimabr <carool.limabr@gmail.com>
2020-10-17 22:22:23 -0300

    privado vs publico, métodos tira_extrato, saca, deposita e transfere

--- oo/conta.py
+++ oo/conta.py
@@ -1,8 +1,30 @@
 #package em Python chama-se module
-class Conta:
+class Conta: #uma classe deve ter uma única responsabilidade (aqui não é para ter método "é inadimplente", por ex.
     def __init__(self, numero, titular, saldo, limite): #método/função construtor. Self é a referência/endereço do objeto
         print("Construindo objeto {}".format(self))
-        self.numero = numero # . significa "acesse"
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero # . significa "acesse" #__ indica private: pode ser usado apenas dentro da classe, ou seja, o atributo recebe o nome da classe como prefixo
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def tira_extrato (self):
+        print("Saldo de {} da(o) titular {}".format(self.__saldo, self.__titular))
+
+    def deposita (self, valor):
+        self.__saldo += valor
+
+    def saca (self, valor):
+        self.__saldo -= valor
+
+    def transfere (self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    #garbage collector: o python procura objetos que não são utilizados e jogam eles no lixo
+    #se quisermos desfazer uma referência,pode-se usar None: >>> outraRef = None
+
+#S - Single responsibility principle
+#O - Open/closed principle
+#L - Liskov substitution principle
+#I - Interface segregation principle
+#D - Dependency inversion principle

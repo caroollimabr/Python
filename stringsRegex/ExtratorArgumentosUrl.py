20825b6af9dfeaadc957b4b263b23ed07b8e0197
caroollimabr <carool.limabr@gmail.com>
2020-11-27 09:47:39 -0300

    manipulação strings

--- stringsRegex/ExtratorArgumentosUrl.py
+++ stringsRegex/ExtratorArgumentosUrl.py
@@ -1,35 +1,51 @@
-class Extrator_Argumentos_Url:
+class ExtratorArgumentosUrl:
     def __init__(self, url):
         if self.url_valida(url):
-            self.url = url
+            self.url = url.lower()
         else:
             raise LookupError("URL inválida.")  # se url estiver vazia, retorne o seguinte erro com a msg
 
     @staticmethod
     def url_valida(url):
-        if url:  # se a url não estiver vazia
+        if url and url.startswith("https://bytebank.com"):  # se a url não estiver vazia
             return True
         else:
             return False
 
     def extrai_argumentos(self):
 
-        busca_moeda_origem = "moedaorigem"
-        busca_moeda_destino = "moedadestino"
+        busca_moeda_origem = "moedaorigem=".lower()
+        busca_moeda_destino = "moedadestino=".lower()
 
         # indice_inicial_moeda_destino = self.url.find("=", 15) + 1  # 15 é o índice
         # indice_inicial_moeda_origem = self.url.find("=") + 1
         # indice_final_moeda_origem = self.url.find("&")
 
-        indice_inicial_moeda_destino = self.encontra_indice_inicial(busca_moeda_destino)
         indice_inicial_moeda_origem = self.encontra_indice_inicial(busca_moeda_origem)
         indice_final_moeda_origem = self.url.find("&")
-
         moeda_origem = self.url[indice_inicial_moeda_origem:indice_final_moeda_origem]
-        moeda_destino = self.url[indice_inicial_moeda_destino:]
+
+        if moeda_origem == "moedadestino":
+            self.troca_moeda_origem()
+            indice_inicial_moeda_origem = self.encontra_indice_inicial(busca_moeda_origem)
+            indice_final_moeda_origem = self.url.find("&")
+            moeda_origem = self.url[indice_inicial_moeda_origem:indice_final_moeda_origem]
+
+        indice_inicial_moeda_destino = self.encontra_indice_inicial(busca_moeda_destino)
+        indice_final_moeda_destino = self.url.find("&valor")
+        moeda_destino = self.url[indice_inicial_moeda_destino:indice_final_moeda_destino]  # quando encontrar "moedadestino=", vai retornar o que vem dps dela
 
         return moeda_origem, moeda_destino
 
     def encontra_indice_inicial(self, moeda_buscada):
-        return self.url.find(moeda_buscada) + len(moeda_buscada) + 1
+        return self.url.find(moeda_buscada) + len(moeda_buscada)
 
+    def troca_moeda_origem(self):
+        self.url = self.url.replace("moedadestino", "real", 1)  # troca a primeira ocorrencia de moedadestino por real
+        print(self.url)
+
+    def extrai_valor(self):
+        busca_valor = "valor="
+        indice_inicial_valor = self.encontra_indice_inicial(busca_valor)
+        valor = self.url[indice_inicial_valor:]  # quando encontrar "valor=", vai retornar o que vem dps dela
+        return valor
--- stringsRegex/main.py
+++ stringsRegex/main.py
@@ -1,12 +1,29 @@
-from ExtratorArgumentosUrl import Extrator_Argumentos_Url
+from ExtratorArgumentosUrl import ExtratorArgumentosUrl
 
-url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar"
-argumentos_url = Extrator_Argumentos_Url(url)
+url = "https://bytebank.com/cambio?moedaoRigem=moedadestino&moedadestino=dolar&valor=1500"
+argumentos_url = ExtratorArgumentosUrl(url)
 moeda_origem, moeda_destino = argumentos_url.extrai_argumentos()
-print(moeda_origem, moeda_destino)
-Extrator_Argumentos_Url.url_valida("a")
+valor = argumentos_url.extrai_valor()
+print(moeda_origem, moeda_destino, valor)
 
+# Extrator_Argumentos_Url.url_valida("a")
 # url = "moedaorigem=real&moedadestino=dolar"
 # index = url.find("moedadestino") + len("moedadestino") + 1
 # substring = url[index:]
 # print(substring)
+
+# string_velha = "bytebytebank"
+# string_nova = string_velha.replace("byte", "carol", 1)  # procura o pedaço da string referenciada e troca 1 vez por "carol" (numa nova variável)
+# print(string_nova)
+
+# banco1 = "bytebank"
+# banco2 = "Bytebank".lower()
+# print(banco1 == banco2)  # python entende diferença entre maiúsculos e minúsculos
+
+url_bytebank = "https://bytebank.com"
+url1 = "https://buscasites.com/busca?q=https://bytebank.com"
+url2 = "https://bitebank.com.br"
+url3 = "https://bytebank.com/cambio/teste/teste"
+
+# print(url2.find(url_bytebank))  # não encontrou
+# print(url3.startswith(url_bytebank))

8338a6ce057e4376af8d20314b524d3bb7daaf41
caroollimabr <carool.limabr@gmail.com>
2021-01-05 10:29:52 -0300

    organizacao em pacote de testes + testes classe Usuario

--- Pytest/src/leilao/dominio.py
+++ Pytest/src/leilao/dominio.py
@@ -1,13 +1,28 @@
 import sys
 
+
 class Usuario:
 
-    def __init__(self, nome):
+    def __init__(self, nome, carteira):
         self.__nome = nome
+        self.__carteira = carteira
+
+    def propoe_lance(self, leilao, valor):
+        if valor > self.__carteira:
+            raise ValueError('Não pode propor um lance com o valor menor que o da carteira.')
+
+        lance = Lance(self, valor)
+        leilao.propoe(lance)
+
+        self.__carteira -= valor
 
     @property
     def nome(self):
         return self.__nome
+
+    @property
+    def carteira(self):
+        return  self.__carteira
 
 
 class Lance:
@@ -25,15 +40,18 @@ class Leilao:
         self.maior_lance = sys.float_info.min  # pega as informações do float (menor valor)
         self.menor_lance = sys.float_info.max  # maior valor
 
-    def propoe(self, lance: Lance): # anotação (type hint), só para ajudar a n se perder no código
+    def propoe(self, lance: Lance):  # anotação (type hint), só para ajudar a n se perder no código
+        # se a lista estiver vazia "not lances"
         # -1: acesso ao último elemento da lista
-        if not self.__lances or self.__lances[-1].usuario != lance.usuario:  # se a lista estiver vazia "not lances"
+        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
             if lance.valor > self.maior_lance:
                 self.maior_lance = lance.valor
             if lance.valor < self.menor_lance:
                 self.menor_lance = lance.valor
 
-        self.__lances.append(lance)
+            self.__lances.append(lance)
+        else:
+            raise ValueError('Erro ao propor lance.')
 
     @property
     def lances(self):
